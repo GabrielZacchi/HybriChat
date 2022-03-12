6802716863d4f97c8f4f2afc7f00269b559fa84d
gabriel.stoffel <gabriel.stoffel@gmail.com>
2022-03-12 18:29:05 -0300

    commit

--- src/components/Login/Content.js
+++ src/components/Login/Content.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Paper, TextField, Button } from "@material-ui/core";
 import Divider from "@mui/material/Divider";
 import VisibilityIcon from "@mui/icons-material/Visibility";
@@ -9,9 +9,15 @@ import Link from "@material-ui/core/Link";
 import { BrowserView, MobileView } from "react-device-detect";
 import logo from "../../assets/hybri.png";
 import GoogleButton from "react-google-button";
+import Carregando from "../carregando";
 
 import { Link as RouterLink, useNavigate } from "react-router-dom";
-import { logInWithEmailAndPassword, signInWithGoogle } from "../../firebase";
+import {
+  logInWithEmailAndPassword,
+  signInWithGoogle,
+  auth,
+} from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const LoginContent = (props) => {
   const [email, setEmail] = useState("");
@@ -20,8 +26,16 @@ const LoginContent = (props) => {
   const [mensagem, setMensagem] = useState("");
   const [open, setOpen] = useState(false);
   const [showPwd, setShowPwd] = useState(false);
+  const [user, loading, error] = useAuthState(auth);
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+    if (user) navigate("/");
+  }, [user, loading]);
 
   const handleEmail = (event) => {
     setEmail(event.target.value);
@@ -61,110 +75,16 @@ const LoginContent = (props) => {
   };
   return (
     <React.Fragment>
-      <div style={{ flexGrow: 1 }}>
-        <MobileView style={{ padding: 20 }}>
-          <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justify="center"
-          >
-            <Grid container spacing={3} align="center" direction="column">
-              <Grid item>
-                <img src={logo} draggable="false" width="180" />
-              </Grid>
-              <Grid item>
-                <TextField
-                  label="Email"
-                  type="email"
-                  fullWidth
-                  value={email}
-                  required
-                  onChange={(e) => handleEmail(e)}
-                />
-              </Grid>
-              <Grid item>
-                <TextField
-                  label="Senha"
-                  type={showPwd ? "text" : "password"}
-                  fullWidth
-                  value={password}
-                  required
-                  onChange={(e) => handlePassword(e)}
-                  onKeyPress={(e) => {
-                    if (e.key === "Enter") {
-                      logInWithEmailAndPassword(email, password);
-                    }
-                  }}
-                  helperText={
-                    <Link component={RouterLink} to="/reset">
-                      Esqueceu sua senha ?
-                    </Link>
-                  }
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="tornar senha visivel"
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                          edge="end"
-                        >
-                          {showPwd ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
-              <Grid item />
-              <Grid item>
-                <Button
-                  type="submit"
-                  color="primary"
-                  variant="contained"
-                  onClick={() => logInWithEmailAndPassword(email, password)}
-                  fullWidth
-                >
-                  Entrar
-                </Button>
-              </Grid>
-              <Grid item>
-                <div>
-                  {`Ainda não tem uma conta? `}
-                  <Link component={RouterLink} to="/register">
-                    Registre-se
-                  </Link>
-                </div>
-              </Grid>
-              <Grid item>
-                <Divider>Ou</Divider>
-              </Grid>
-              <Grid item>
-                <GoogleButton
-                  label="Entrar com Google"
-                  style={{
-                    width: "100%",
-                  }}
-                  onClick={() => {
-                    signInWithGoogle();
-                  }}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-        </MobileView>
-        <BrowserView style={{ backgroundColor: "#36393f" }}>
-          <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justify="center"
-            style={{ minHeight: "100vh" }}
-          >
-            <Paper elevation={10} style={paperStyle}>
+      {!loading ? (
+        <div style={{ flexGrow: 1 }}>
+          <MobileView style={{ padding: 20 }}>
+            <Grid
+              container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              justify="center"
+            >
               <Grid container spacing={3} align="center" direction="column">
                 <Grid item>
                   <img src={logo} draggable="false" width="180" />
@@ -252,10 +172,114 @@ const LoginContent = (props) => {
                   />
                 </Grid>
               </Grid>
-            </Paper>
-          </Grid>
-        </BrowserView>
-      </div>
+            </Grid>
+          </MobileView>
+          <BrowserView style={{ backgroundColor: "#36393f" }}>
+            <Grid
+              container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              justify="center"
+              style={{ minHeight: "100vh" }}
+            >
+              <Paper elevation={10} style={paperStyle}>
+                <Grid container spacing={3} align="center" direction="column">
+                  <Grid item>
+                    <img src={logo} draggable="false" width="180" />
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      label="Email"
+                      type="email"
+                      fullWidth
+                      value={email}
+                      required
+                      onChange={(e) => handleEmail(e)}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      label="Senha"
+                      type={showPwd ? "text" : "password"}
+                      fullWidth
+                      value={password}
+                      required
+                      onChange={(e) => handlePassword(e)}
+                      onKeyPress={(e) => {
+                        if (e.key === "Enter") {
+                          logInWithEmailAndPassword(email, password);
+                        }
+                      }}
+                      helperText={
+                        <Link component={RouterLink} to="/reset">
+                          Esqueceu sua senha ?
+                        </Link>
+                      }
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <IconButton
+                              aria-label="tornar senha visivel"
+                              onClick={handleClickShowPassword}
+                              onMouseDown={handleMouseDownPassword}
+                              edge="end"
+                            >
+                              {showPwd ? (
+                                <VisibilityIcon />
+                              ) : (
+                                <VisibilityOffIcon />
+                              )}
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </Grid>
+                  <Grid item />
+                  <Grid item>
+                    <Button
+                      type="submit"
+                      color="primary"
+                      variant="contained"
+                      onClick={() => logInWithEmailAndPassword(email, password)}
+                      fullWidth
+                    >
+                      Entrar
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <div>
+                      {`Ainda não tem uma conta? `}
+                      <Link component={RouterLink} to="/register">
+                        Registre-se
+                      </Link>
+                    </div>
+                  </Grid>
+                  <Grid item>
+                    <Divider>Ou</Divider>
+                  </Grid>
+                  <Grid item>
+                    <GoogleButton
+                      label="Entrar com Google"
+                      style={{
+                        width: "100%",
+                      }}
+                      onClick={() => {
+                        signInWithGoogle();
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Grid>
+          </BrowserView>
+        </div>
+      ) : (
+        <div style={{ flexGrow: 1 }}>
+          <Carregando />
+        </div>
+      )}
     </React.Fragment>
   );
 };
